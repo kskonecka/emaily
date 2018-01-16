44e68e77bda6fc809375ead8cb78cc45606101b8
Kasia Skonecka <kask@unisport.dk>
2018-01-16 12:40:32 +0100

    initial commit

---
+++ index.js
@@ -0,0 +1,14 @@
+// not this one:
+// import express from 'express' because at the time Node JS had no support for ES2015 modules
+// on the react side of things we will use import express from 'express'
+// down: common js modules
+const express = require('express');
+const app = express();
+
+// app is the express server
+app.get('/', (req, res) => {
+  res.send({ hi: 'there'});
+});
+
+const PORT = process.env.PORT || 5000
+app.listen(PORT);
