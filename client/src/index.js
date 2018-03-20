b05d2956a3d14ba028c20202a2dc582800d8c9cb
Kasia Skonecka <kask@unisport.dk>
2018-03-20 16:04:00 +0100

    initial redux syntax

--- client/src/index.js
+++ client/src/index.js
@@ -1,8 +1,19 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import "materialize-css/dist/css/materialize.min.css";
+import React from "react";
+import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
+import { createStore, applyMiddleware } from "redux";
+import reduxThunk from "redux-thunk";
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+import App from "./components/App";
+import reducers from "./reducers";
+
+const store = createStore(reducers, {}, applyMiddleware(reduxThunk));
+
+// <Provider /> is a react component that makes the {store} accessible to every component in the app
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.querySelector("#root")
+);
