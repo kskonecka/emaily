657d42c476aa28970fc611307d5f28b9c4aabb60
kskonecka <kasia.skonecka@gmail.com>
2018-04-29 21:24:20 +0200

    tweaks for prod deployment

--- index.js
+++ index.js
@@ -25,5 +25,16 @@ app.use(passport.session());
 require('./routes/authRoutes')(app); //instead of: require('./routes/authRoutes');
 require('./routes/billingRoutes')(app);
 
+if (process.env.NODE_ENV === 'production') {
+  // Express will serve up prod assets like main.js or main.css
+  app.use(express.static('client/build'))
+
+  // express will serve up the index.html file if it doesnt recognize the route
+  const path = require('path');
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+  });
+}
+
 const PORT = process.env.PORT || 5000
 app.listen(PORT);
