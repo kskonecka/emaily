4d24fa1b7f21693f7c0ea6c2758997c0661f3940
Kasia <kasia.skonecka@gmail.com>
2018-07-29 21:59:53 +0200

    users can see surveys results

--- middlewares/requireLogin.js
+++ middlewares/requireLogin.js
@@ -2,6 +2,5 @@ module.exports = (req, res, next) => {
   if (!req.user) {
     return res.status(401).send({ error: 'You must log in!' });
   }
-
   next(); //everything looks good, let this user continue on to the next handler
 };
